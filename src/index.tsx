9e5973f9d08aed3e2f3e9188d383b315d63d497c
vadimcrmnl1 <you@example.com>
2023-10-08 18:10:04 +0300

    add ErrorPage.tsx, fix Routes.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -12,9 +12,10 @@ const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <Provider store={store}>
-        <RouterProvider router={router}/>
-    </Provider>
+        <Provider store={store}>
+            <RouterProvider router={router}/>
+        </Provider>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
